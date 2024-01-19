690daf8d52cd9b60077e64fb39ac38359200b8b1
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-19 15:55:21 -0300

    refactor: clean and organize project

---
+++ lib/main_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:chat_flutter/pages/main/main_page.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
