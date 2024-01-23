42aa20011094fd53039ecd172f42fe06955234f1
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-22 21:49:39 -0300

    feat: install and configure firebase and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:chat_flutter/main_app.dart';
+import 'package:chat_flutter/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
