be1969ec8e84cfea5a009e9686a8fa3ba487fc3e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-22 23:07:35 -0300

    feat: create chat services

---
+++ lib/services/chat_service.dart
@@ -0,0 +1,7 @@
+import 'package:chat_flutter/models/chat_model.dart';
+
+abstract class ChatService {
+  Future<void> create(String name, String category);
+
+  Future<List<ChatModel>> list();
+}
---
+++ lib/services/impl/firebase_chat_service.dart
@@ -0,0 +1,21 @@
+import 'package:chat_flutter/models/chat_model.dart';
+import 'package:chat_flutter/services/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseChatService implements ChatService {
+  final FirebaseFirestore _db;
+
+  FirebaseChatService(this._db);
+
+  @override
+  Future<void> create(String name, String category) async {
+    var newChat = ChatModel(name: name, category: category);
+    await _db.collection("chats").add(newChat.toJson());
+  }
+
+  @override
+  Future<List<ChatModel>> list() async {
+    var snapshot = await _db.collection("chats").get();
+    return snapshot.docs.map((doc) => ChatModel.fromJson(doc.data())).toList();
+  }
+}
